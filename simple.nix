daf1cc77b8bf736089858da1980f86a6a76716a1
Saikat Das <saikatdas0790@gmail.com>
2025-04-13 16:00:01 +0000

    Refactor derivation to inherit gcc and coreutils from pkgs

--- simple.nix
+++ simple.nix
@@ -5,8 +5,7 @@ derivation {
   name = "simple";
   builder = "${pkgs.bash}/bin/bash";
   args = [ ./simple_builder.sh ];
-  gcc = pkgs.gcc;
-  coreutils = pkgs.coreutils;
+  inherit (pkgs) gcc coreutils;
   src = ./simple.c;
   system = builtins.currentSystem;
 }
